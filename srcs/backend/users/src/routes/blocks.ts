9866cb28c31d04944d04376939219881f0029c3b
Aleksei Kurochkin <ku.alexej@gmail.com>
2025-09-26 15:25:03 +0200

    block: routes, controller

--- srcs/backend/users/src/routes/blocks.ts
+++ srcs/backend/users/src/routes/blocks.ts
@@ -1,30 +1,50 @@
 import { FastifyInstance } from "fastify";
+import { verifyJWT } from "../middleware/verifyJWT";
+import { BlockController } from "../controllers/BlockController";
+import { UserIdParams, userIdSchema } from "../schemas/blocks";
 
 export default async function blocksRoutes(fastify: FastifyInstance) {
+	const blockController = new BlockController(fastify.services.blocks);
 
-  // List blocked users [Requires user authentication] 
-  fastify.get("/", async (request, reply) => {
-    // get blocked list
-    return { ok: true };
-  });
-  
-  // Block user [Requires user authentication] 
-  fastify.post(
-    "/:id",
-    async (request, reply) => {
-    });
+	// List blocked users [Requires user authentication] 
+	fastify.get(
+		"/",
+		{
+			preHandler: verifyJWT
+		},
+		blockController.getBlockedUsers.bind(blockController)
+		// get blocked list
+	);
+	
+	// Block user [Requires user authentication] 
+	fastify.post<{ Params: UserIdParams }>(
+		"/:id",
+		{
+			schema: { params: userIdSchema },
+			preHandler: verifyJWT
+		},
+		blockController.blockUser.bind(blockController)
+		// expected param: id (user to block)
+	);
 
-  // Unblok user [Requires user authentication]
-  fastify.delete(
-    "/:id",
-    async (request, reply) => {
-  });
+	// Unblok user [Requires user authentication]
+	fastify.delete<{ Params: UserIdParams }>(
+		"/:id",
+		{
+			schema: { params: userIdSchema },
+			preHandler: verifyJWT
+		},
+		blockController.unblockUser.bind(blockController)
+	);
 
-  // Check if blocked [Requires internal service authentication]
-  fastify.post(
-    "/check",
-    async (request, reply) => {
-      // expected body: {user_id: number, target_id: number}
-  });
+	// Check if blocked [Requires internal service authentication]
+	fastify.get<{ Params: UserIdParams }>(
+		"/check/:id",
+		{
+			schema: { params: userIdSchema },
+			preHandler: verifyJWT
+		},
+		blockController.isBlocked.bind(blockController)
+	);
 
 }
